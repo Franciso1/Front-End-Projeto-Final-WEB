19ad0f214fa876ef2aad49da3842ced41cddbc58
Francisco1 <franciscocarlosmateus@yahoo.com.br>
2025-07-07 20:33:52 -0300

    Correcao

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -9,7 +9,7 @@ import { Contato } from '../models/contact';
 export class ContactService {
 
   // A URL da sua API. Verifique se a porta está correta.
-  private apiUrl = 'http://capybaraagenda.duckdns.org';
+  private apiUrl = 'https://capybaraagenda.duckdns.org';
 
   constructor(private http: HttpClient) { }
 
